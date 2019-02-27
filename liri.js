5a2a5daabf6bfe03ad50567ddf6881755c1fc6e1
Nicole W <nicoleewhitee@gmail.com>
2019-02-27 14:37:16 -0800

    for loop added

--- liri.js
+++ liri.js
@@ -5,45 +5,73 @@ var keys = require("./keys.js");
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var spotifyAPI = new Spotify(keys.spotify);
-
-// var BandsInTown = require('bandsInTown');
-var request = require('request')
+var moment = require("moment");
 var fs = require("fs");
 
+var commands = process.argv[2];
 
+var nodeArgs = process.argv;
+var info = "";
 
- 
-spotifyAPI.search({ type: 'track', query: 'Redbone' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data.tracks.items[0]); 
-});
+for (var i=3; i<nodeArgs.length; i++) {
+    if (i>3 && i<nodeArgs.length){
+        info = info + "+" + nodeArgs[i];
+    } else {
+        info += nodeArgs[i];
+    }
+}
+// var BandsInTown = require('bandsInTown');
+// var request = require('request')
+
 
 var givenCommands = function () {
-    if (command === "spotify-this-song") {
+    if (commands === "spotify-this-song") {
         if (info === "") {
             info = "Redbone"
         }
+        spotifyAPI.search({ type: 'track', query: 'media', limit: 1 }, function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            }
+
+    console.log("Artist: " + data.tracks.items[0].artists[0].name);
+    console.log("Song Name: " + data.tracks.items[0].name);
+    console.log("Preview Link: " + data.tracks.items[0].external_urls.spotify);
+    console.log("Album: " + data.tracks.items[0].album.name);
+    });
+
+} else if (command === "movie-this") {
+        if (info === "") {
+            info = "Mr. Nobody";
+        }
+
+        var queryUrl = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy";
+
+        axios.get(queryUrl).then(
+            function(response) {
+                console.log("Title: " + response.data.Title);
+                console.log("Movie Year: " + response.data.Year);
+                console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+                console.log("Rotten Tomatoes Ratings: " + response.data.Ratings[1].Value);
+                console.log("Country Origin: " + response.data.Country);
+                console.log("Movie Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors List: " +response.data.Actors);
+            }
+        );
+
+        console.log("Venue: " + response.data[0].venue.name);
+        console.log("Venue Location " + response.data[0].venue.city);
+        console.log("Date of Event: " + moment(eventTime).format("MM/DD/YYYY"));
+
+
+        commands = dataArr[0];
+        info = dataArr[1];
+        runGivenCommands();
+        });
+
     }
+
 }
-
-//need to add these liri commands?
-// concert-this
-// spotify-this-song 
-// movie-this 
-// do-what-it-says
-
-// var bandsInTown2 = process.argv[2]
-// Name of the venue
-// Venue location
-// Date of the Event (use moment to format this as "MM/DD/YYYY")
-
-// bandsInTown2.find({search: bandsInTown, name: "venue", location: "place", date: "MM/DD/YYYY"}, function(err, result) {
-//     if (err) console.log(err)
-
-//     console.log(JSON.stringify(result, null, 2));
-// });
 
 runGivenCommands();
